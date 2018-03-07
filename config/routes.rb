bf84a8326381729eae9d9a5a94cc5eb23950dd8a
PJ Frias <PeejFrias@Gmail.com>
2018-03-06 21:24:12 -0500

    add home index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :groups
-  resources :prayers
+  
+  resources :prayers do
+    resources :comments
+  end
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
