e981e8362bac1d5d229087879ceb3205b1c6b6df
PJ Frias <PeejFrias@Gmail.com>
2018-03-06 20:58:00 -0500

    add prayers migration

--- db/migrate/20180307015413_create_prayers.rb
+++ db/migrate/20180307015413_create_prayers.rb
@@ -2,6 +2,12 @@ class CreatePrayers < ActiveRecord::Migration[5.1]
   def change
     create_table :prayers do |t|
       t.text :details
+      t.string :overview
+      t.integer :author_id
+      t.boolean :anonymous?, default: false
+      t.boolean :public?, default: false
+      t.datetime :created_at
+      t.datetime :updated_at
 
       t.timestamps
     end
