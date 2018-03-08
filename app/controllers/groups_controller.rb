4f6672ced7392c353858b3cdfc7bcaf8a270b04b
PJ Frias <PeejFrias@Gmail.com>
2018-03-08 11:24:03 -0500

    add prayer new and edit pages, and added prayer controller action to create new prayer

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,47 @@
 class GroupsController < ApplicationController
+
+  def index
+    @groups = Group.all
+  end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    group = Group.new(group_params)
+    group.leader = current_user
+    group.members << current_user
+
+    if group.save
+      flash[:notice] = "Successfully started a new prayer group"
+      redirect_to groups_path
+    else
+      flash[:alert] = "Prayer group requires a name"
+      redirect_to new_group_path
+    end
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+
+  end 
+
+  def delete
+    @group = Group.find(params[:id])
+  end
+
+  private
+
+  def group_params
+    params.require("group").permit(:name, :description, :is_public)
+  end
+
 end
